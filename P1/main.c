bda3fb30d81a55ec5ed539596ab12c2b771632f9
AresiRosende <a.rosendecoto@gmail.com>
2023-04-12 14:48:49 +0200

    P1 funcional

--- P1/main.c
+++ P1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
 
 void inicializaCadena(char *cadena, int n){
   int i;
@@ -25,23 +26,39 @@ int main(int argc, char *argv[])
     exit(1); 
   }
     
-  int i, n, count=0;
+  int countrec, numprocs, rank, i, n, count=0;
   char *cadena;
   char L;
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+  MPI_Status status;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   n = atoi(argv[1]);
   L = *argv[2];
   
   cadena = (char *) malloc(n*sizeof(char));
   inicializaCadena(cadena, n);
-  
-  for(i=0; i<n; i++){
-    if(cadena[i] == L){
-      count++;
+
+
+  if (rank > 0) {
+    for (i = rank-1; i <= n; i += numprocs-1){
+      if(cadena[i] == L){
+        count++;
+      }
     }
+    MPI_Send(&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
   }
-  
-  printf("El numero de apariciones de la letra %c es %d\n", L, count);
-  free(cadena);
-  exit(0);
+  else {
+    for (int k = 1; k < numprocs; k++){
+      MPI_Recv(&countrec, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      count += countrec;
+    }
+    printf("El numero de apariciones de la letra %c es %d\n", L, count);
+    free(cadena);
+  }
+
+  MPI_Finalize();
 }
