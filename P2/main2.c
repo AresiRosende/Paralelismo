f356c1b4619c2845a4352391b361a263955f271c
dferreirovillamor <daniel.ferreiro.villamor@gmail.com>
2023-04-18 17:07:06 +0200

    Minor fixes & Verification code added

--- P2/main2.c
+++ P2/main2.c
@@ -22,6 +22,7 @@ void inicializaCadena(char *cadena, int n){
 int MPI_FlattreeBCast(void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm){
   int result, numprocs, rank;
   int *aux;
+  int *aux2;
 
     MPI_Comm_size(comm, &numprocs);
     MPI_Comm_rank(comm, &rank);
@@ -33,20 +34,17 @@ int MPI_FlattreeBCast(void *sendbuf, void *recvbuf, int count, MPI_Datatype data
       }
     } else {
       aux = ((int *) sendbuf); //add the value of process with rank 0
+      //Esto lo hago para solucionar un problema de gestion de memoria si igualasemos contenidos en la linea anterior (asi el count individual de cada proceso no se modifica al terminar la operacion)
+      *aux2 = *aux;
       for (int k = 1; k < numprocs; k++){
         result = MPI_Recv(recvbuf, count, datatype, k, MPI_ANY_TAG, comm, MPI_STATUS_IGNORE);
         if (result != MPI_SUCCESS) return result;
-        *aux += *((int *) recvbuf);
+        *aux2 += *((int *) recvbuf);
       }
-      recvbuf = aux;
+      *((int *)recvbuf) = *aux2;
     }
     return MPI_SUCCESS;
 }
-
-
-//WIP
-
-
 
 
 int MPI_BinomialBCast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm){
@@ -60,6 +58,8 @@ int MPI_BinomialBCast(void *buffer, int count, MPI_Datatype datatype, int root, 
   for(int i = 1; ; i*=2){
     if(rank < i) {
       if (i+rank >= numprocs) break;
+      //comprobacion Binomial-----------------------------------------------------------------------------------
+      //printf("rank %d (sending to) -> rank %d\n", rank, i+rank);
       result = MPI_Send(buffer, count, datatype, i+rank, 0, comm);
     }
   }
@@ -88,11 +88,17 @@ int main(int argc, char *argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  n = atoi(argv[1]);
-  L = *argv[2];
+  //Ponemos los valores de n y l para los procesos con rank > 0 mal inicialmente para poder verificar el correcto funcionamiento del MPI_BinomialBCast
+  if(rank == 0){
+    n = atoi(argv[1]);
+    L = *argv[2];
+  }else{
+    n = 0;
+    L = 'Z';
+  }
 
-  //MPI_BinomialBCast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  //MPI_BinomialBCast(&L, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+  MPI_BinomialBCast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_BinomialBCast(&L, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
   MPI_Bcast(&count, 1, MPI_INT, rank, MPI_COMM_WORLD);
 
   cadena = (char *) malloc(n*sizeof(char));
@@ -108,7 +114,9 @@ int main(int argc, char *argv[])
   MPI_FlattreeBCast(&count, &countrec, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
   //MPI_Reduce(&count, &countrec, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-  if (rank == 0) printf("El numero de apariciones de la letra %c es %d\n", L, count);
+  if (rank == 0) printf("El numero de apariciones de la letra %c es %d\n", L, countrec);
+  //Para ensenar los counts individuales de cada proceso
+  //printf("rank %d count-> %d\n",rank, count);
   free(cadena);
 
   MPI_Finalize();
