9b1cf9278419427b29421fa4d8c7294affd06d2c
AresiRosende <a.rosendecoto@gmail.com>
2023-04-18 12:29:16 +0200

    El Flattree va bien creo

--- P2/main2.c
+++ P2/main2.c
@@ -19,21 +19,26 @@ void inicializaCadena(char *cadena, int n){
   }
 }
 
-int MPI_FlattreeBCast(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op, int root, MPI_Comm comm){
+int MPI_FlattreeBCast(void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm){
   int result, numprocs, rank;
+  int *aux;
 
     MPI_Comm_size(comm, &numprocs);
     MPI_Comm_rank(comm, &rank);
 
-    if (rank == root) {
-    for (int i = 0; i < numprocs; i++)
-            if (i != root) {
-                result = MPI_Send(buffer, count, datatype, i, 0, comm);
-                if (error != MPI_SUCCESS) return error;
-            }
+    if (rank != root) {
+      for (int i = 1; i < numprocs; i++){
+        result = MPI_Send(sendbuf, count, datatype, root, 0, comm);
+        if (result != MPI_SUCCESS) return result;
+      }
     } else {
-        error = MPI_Recv(buffer, count, datatype, root, 0, comm, &status);
-        if (error != MPI_SUCCESS) return error;
+      aux = ((int *) sendbuf); //add the value of process with rank 0
+      for (int k = 1; k < numprocs; k++){
+        result = MPI_Recv(recvbuf, count, datatype, k, MPI_ANY_TAG, comm, MPI_STATUS_IGNORE);
+        if (result != MPI_SUCCESS) return result;
+        *aux += *((int *) recvbuf);
+      }
+      recvbuf = aux;
     }
     return MPI_SUCCESS;
 }
@@ -68,49 +73,43 @@ int MPI_BinomialBCast(void *buffer, int count, MPI_Datatype datatype, int root, 
 
 int main(int argc, char *argv[])
 {   
+
+  if(argc != 3){
+    printf("Numero incorrecto de parametros\nLa sintaxis debe ser: program n L\n  program es el nombre del ejecutable\n  n es el tamaño de la cadena a generar\n  L es la letra de la que se quiere contar apariciones (A, C, G o T)\n");
+    exit(1); 
+  }
+
   int countrec, numprocs, rank, i, n, count=0;
   char *cadena;
   char L;
+
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  while (1) {
-    if (rank == 0){
-        printf("Enter string length (0 to exit):\n");
-        scanf("%d", &n);
-        printf("Enter character to count (A, C, G o T):\n");
-        scanf("%c", &L);
-    }
+  n = atoi(argv[1]);
+  L = *argv[2];
 
-    if (n == 0) break;
+  //MPI_BinomialBCast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  //MPI_BinomialBCast(&L, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&count, 1, MPI_INT, rank, MPI_COMM_WORLD);
 
-    MPI_BinomialBCast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_BinomialBCast(&L, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&count, 1, MPI_INT, rank, MPI_COMM_WORLD);
-
-    count = 0;
-    counrec = 0;
-
-    cadena = (char *) malloc(n*sizeof(char));
-    inicializaCadena(cadena, n);
+  cadena = (char *) malloc(n*sizeof(char));
+  inicializaCadena(cadena, n);
 
 
-    if (rank > 0) {
-     for (i = rank-1; i <= n; i += numprocs-1){ 
-         if(cadena[i] == L){
-         count++;
-       }
-     }
-    }
+  for (i = rank; i <= n; i += numprocs){ 
+    if(cadena[i] == L){
+    count++;
+   }
+  }
 
-    MPI_FlattreeBCast(&count, &countrec, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    //MPI_Reduce(&count, &countrec, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    if (rank == 0) printf("El numero de apariciones de la letra %c es %d -> %d\n", L, count, rank);
-    free(cadena);
-    }
+  MPI_FlattreeBCast(&count, &countrec, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  //MPI_Reduce(&count, &countrec, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
+  if (rank == 0) printf("El numero de apariciones de la letra %c es %d\n", L, count);
+  free(cadena);
 
   MPI_Finalize();
 }
